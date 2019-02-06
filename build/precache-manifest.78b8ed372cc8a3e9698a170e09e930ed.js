4d55d3cd346faec7d36c48ebd90847fe758bb844
unknown <umetov.atai@gmail.com>
2019-02-06 22:29:18 +0600

    v 1.0.0

---
+++ build/precache-manifest.78b8ed372cc8a3e9698a170e09e930ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "526d9427a7bfe8b0731a1f5347bcd7b5",
+    "url": "/static/media/medicine.526d9427.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b25d732d68904fb0342",
+    "url": "/static/js/main.5b25d732.chunk.js"
+  },
+  {
+    "revision": "1e44253f9ed70e19d7fb",
+    "url": "/static/js/1.1e44253f.chunk.js"
+  },
+  {
+    "revision": "5b25d732d68904fb0342",
+    "url": "/static/css/main.393adb76.chunk.css"
+  },
+  {
+    "revision": "1e44253f9ed70e19d7fb",
+    "url": "/static/css/1.94cf2afe.chunk.css"
+  },
+  {
+    "revision": "6eb28f6ef3d50745dbe158c7bef8e1c1",
+    "url": "/index.html"
+  }
+];
